b336f51eb8bf68cc4250e3050ac6b6b0e200b7f3
Jason <Jason@kyousatoshitekiMacBook-Air.local>
2015-02-03 19:23:47 -0800

    Initial Commit

---
+++ Program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program
+//
+//  Created by Jason on 2/3/15.
+//  Copyright (c) 2015 Jason. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
